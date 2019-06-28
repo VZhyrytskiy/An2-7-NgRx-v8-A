e02bb286f70c13f44212749a18138e5d1fb55cb9
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-06-28 10:03:46 +0300

    install redux dev tools

--- src/app/core/@ngrx/core-store.module.ts
+++ src/app/core/@ngrx/core-store.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 
 // @Ngrx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { metaReducers } from './meta-reducers';
+import { environment } from './../../../environments/environment';
 
 @NgModule({
   declarations: [],
@@ -20,7 +22,9 @@ import { metaReducers } from './meta-reducers';
           strictActionSerializability: true
         }
       }
-    )
+    ),
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ]
 })
 export class CoreStoreModule {}
