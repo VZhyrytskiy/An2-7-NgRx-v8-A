537b368a16884ec916cf9b132b8f60f1543c9c3e
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-06-30 12:22:32 +0300

    router state

--- src/app/core/@ngrx/core-store.module.ts
+++ src/app/core/@ngrx/core-store.module.ts
@@ -5,26 +5,32 @@ import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { metaReducers } from './meta-reducers';
+import { routerReducers, CustomSerializer } from './router';
 import { environment } from './../../../environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot(
-      {},
-      {
-        metaReducers,
-        runtimeChecks: {
-          strictStateImmutability: true,
-          strictActionImmutability: true,
-          strictStateSerializability: true,
-          strictActionSerializability: true
-        }
+    StoreModule.forRoot(routerReducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        // router state is not serializable
+        // set false if you don't use CustomSerializer
+        strictStateSerializability: true,
+        // router action is not serializable
+        // set false
+        strictActionSerializability: false
       }
-    ),
+    }),
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({
+      serializer: CustomSerializer
+    }),
     // Instrumentation must be imported after importing StoreModule (config is optional)
     !environment.production ? StoreDevtoolsModule.instrument() : []
   ]
