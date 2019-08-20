ecb4865a61c1312261d8056377857d61064afc69
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-08-20 08:39:56 +0300

    refactor: include TasksEffects into TasksStoreModule

--- src/app/core/@ngrx/tasks/tasks-store.module.ts
+++ src/app/core/@ngrx/tasks/tasks-store.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 // @NgRx
 import { StoreModule } from '@ngrx/store';
 import { tasksReducer } from './tasks.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { TasksEffects } from './tasks.effects';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, StoreModule.forFeature('tasks', tasksReducer)]
+  imports: [
+    CommonModule,
+    StoreModule.forFeature('tasks', tasksReducer),
+    EffectsModule.forFeature([TasksEffects])
+  ]
 })
 export class TasksStoreModule {}
