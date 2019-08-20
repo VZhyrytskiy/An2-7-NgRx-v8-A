bfc7f756ab1f3c25b948a8f17feb35af27ed2731
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-08-20 13:02:54 +0300

    feature: RouerState.Minimal

--- src/app/core/@ngrx/root-store.module.ts
+++ src/app/core/@ngrx/root-store.module.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreRouterConnectingModule, RouterState } from '@ngrx/router-store';
 import { TasksStoreModule } from './tasks/tasks-store.module';
 import { metaReducers } from './meta-reducers';
 import { routerReducers, CustomSerializer } from './router';
@@ -25,12 +25,14 @@ import { environment } from './../../../environments/environment';
         strictStateSerializability: true,
         // router action is not serializable
         // set false
-        strictActionSerializability: false
+        strictActionSerializability: true
       }
     }),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot({
-      serializer: CustomSerializer
+      stateKey: 'router',
+      routerState: RouterState.Minimal
+      // serializer: CustomSerializer
     }),
     // Instrumentation must be imported after importing StoreModule (config is optional)
     TasksStoreModule,
